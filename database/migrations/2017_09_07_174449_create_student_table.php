b0fe13a474e4b795d9d6dfa355c32c062761103e
Pratima777 <pratima@oryxcloud.com>
2017-09-08 10:04:38 +0530

    Migration for table

---
+++ database/migrations/2017_09_07_174449_create_student_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('sname');
+            $table->integer('age');
+            $table->integer('class');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
